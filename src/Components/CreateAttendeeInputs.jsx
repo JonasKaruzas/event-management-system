161eed75fc9a171f8a7986e94933d81509d56d1a
JonasKaruzas <jonas.karuzas@gmail.com>
2022-11-28 19:38:42 +0200

    feat: :sparkles: CreateAttendeInputs element form submitHandler works

--- src/Components/CreateAttendeeInputs.jsx
+++ src/Components/CreateAttendeeInputs.jsx
@@ -1,5 +1,34 @@
+import { useRef } from "react";
+
 export function CreateAttendeeInputs() {
+
+  const firstName = useRef();
+  const lastName = useRef();
+  const email = useRef();
+  const age = useRef();
+  
+  function submitHandler(e) {
+    e.preventDefault();
+
+    const newAttendee = {
+      firstName: firstName.current.value,
+      lastName: lastName.current.value,
+      email: email.current.value,
+      age: age.current.value,
+    }
+
+    console.log(newAttendee);
+
+    e.target.reset();
+  }
+
   return (
-    <div>I Create Attendee Inputs</div>
+      <form onSubmit={submitHandler}> 
+        <input ref={firstName} type='text' name='firstName' placeholder="First name" required />
+        <input ref={lastName} type='text' name='lastName' placeholder="Last name" required />
+        <input ref={email} type='email' name='email' placeholder="Email" required />
+        <input ref={age} type='number' name='age' placeholder="Age" required />
+        <button>Submit</button>
+      </form>
   )
 }

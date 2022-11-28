8adf37ad9c371381075ee5a7eba5ba1ac99d0e1b
JonasKaruzas <jonas.karuzas@gmail.com>
2022-11-28 23:47:41 +0200

    style: :lipstick: Delete and Edit button styles

--- src/Components/AttendeeListItem.jsx
+++ src/Components/AttendeeListItem.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import deleteImg from "../Assets/Images/delete.svg";
+import pencilImg from "../Assets/Images/pencil.svg";
 
 const Card = styled.div`
   width: 100%;
@@ -33,6 +35,26 @@ const Age = styled.div`
   font-size: 15px;
 `
 
+const Button = styled.button`
+  border: none;
+  background-color: ${props => props.delete && '#EB6440'};
+  background-color: ${props => props.edit && '#497174'};
+  border-radius: 4px;
+  cursor: pointer;
+  opacity: 0.8;
+  
+  :hover {
+    opacity: 1;
+  }
+
+  img {
+    filter: invert(1);
+    // padding: 2px;
+    height: 28px;
+    width: 35px;
+  }
+`
+
 
 
 export function AttendeeListItem( {attendee} ) {
@@ -41,6 +63,10 @@ export function AttendeeListItem( {attendee} ) {
       <Name>{attendee.firstName} {attendee.lastName}</Name>
       <Email>{attendee.email}</Email>
       <Age>{attendee.age}</Age>
+      <div>
+        <Button edit style={{marginRight: 10}}><img src={pencilImg} alt='edit' /></Button>
+        <Button delete><img src={deleteImg} alt='delete' /></Button>
+      </div>
     </Card>
     )
   }

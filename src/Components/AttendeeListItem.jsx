7145433bcf4905adf66d70cf290698daffb1e02e
JonasKaruzas <jonas.karuzas@gmail.com>
2022-11-28 21:29:34 +0200

    style: :lipstick: AttendeeLIstItem styling

--- src/Components/AttendeeListItem.jsx
+++ src/Components/AttendeeListItem.jsx
@@ -1,11 +1,46 @@
+import styled from "styled-components"
+
+const Card = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 30px;
+  padding: 5px;
+
+  :nth-child(odd) {
+    background-color: #EFF5F5;
+  }
+
+  :hover {
+    color: #EB6440;
+  }
+`
+
+const Name = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+`
+
+const Email = styled.div`
+  font-weight: 400;
+  font-size: 15px;
+`
+
+const Age = styled.div`
+  font-weight: 400;
+  font-size: 15px;
+`
+
+
+
 export function AttendeeListItem( {attendee} ) {
   return (
-    <div>
-      <div>{attendee.firstName}</div>
-      <div>{attendee.lastName}</div>
-      <div>{attendee.email}</div>
-      <div>{attendee.age}</div>
-    </div>
+    <Card>
+      <Name>{attendee.firstName} {attendee.lastName}</Name>
+      <Email>{attendee.email}</Email>
+      <Age>{attendee.age}</Age>
+    </Card>
     )
   }
   
